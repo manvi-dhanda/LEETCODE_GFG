5e163210afa7e24dc58f9db40005092a7ea2f193
Manvi Dhanda <149007210+manvi-dhanda@users.noreply.github.com>
2026-02-17 11:26:53 +0530

    Time: 0 ms (100%), Space: 10.8 MB (41.74%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int sz;
+    vector<vector<int>> ans;
+
+    void func(vector<int>& nums, int ind)
+    {
+        if(ind >= sz)
+        {
+            ans.push_back(nums);
+            return;
+        }
+
+        for(int i = ind; i < sz; i++)   // FIXED HERE
+        {
+            swap(nums[ind], nums[i]);
+            func(nums, ind + 1);
+            swap(nums[ind], nums[i]);
+        }
+    }
+
+    vector<vector<int>> permute(vector<int>& nums) {
+        sz = nums.size();
+        func(nums, 0);
+        return ans;
+    }
+};
