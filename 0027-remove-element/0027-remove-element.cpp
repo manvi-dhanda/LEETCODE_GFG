b2a705b725e86d06a0804244a860d1850aa234c0
Manvi Dhanda <149007210+manvi-dhanda@users.noreply.github.com>
2026-02-20 00:34:56 +0530

    Time: 0 ms (100%), Space: 11.8 MB (11.83%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        int k = 0; // count of elements not equal to val
+        int k = 0; 
         
         for (int i = 0; i < nums.size(); i++) {
             if (nums[i] != val) {
-                nums[k] = nums[i]; // move the non-val element to the front
+                nums[k] = nums[i]; 
                 k++;
             }
         }
         
-        return k; // return count of elements not equal to val
+        return k; 
     }
 };
