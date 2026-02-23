600d90d50479d4f589f5ad5212621cbb8cb52d95
Manvi Dhanda <149007210+manvi-dhanda@users.noreply.github.com>
2026-02-23 14:39:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Common in 3 Sorted Arrays/common-in-3-sorted-arrays.cpp
@@ -0,0 +1,39 @@
+class Solution {
+  public:
+
+    vector<int> commonElements(vector<int> &arr1, vector<int> &arr2,
+                               vector<int> &arr3) {
+        // Code Here
+        int i = 0, j = 0, k = 0;
+      int n1 = arr1.size(), n2 = arr2.size(), n3 = arr3.size();
+    
+      vector<int> common;
+
+    while (i < n1 && j < n2 && k < n3)
+    {
+        if (arr1[i] == arr2[j] && arr2[j] == arr3[k])
+        {
+            common.push_back(arr1[i]);
+            i++;
+            j++;
+            k++;
+
+            while (i < n1 && arr1[i] == arr1[i - 1])
+                i++;
+            while (j < n2 && arr2[j] == arr2[j - 1])
+                j++;
+            while (k < n3 && arr3[k] == arr3[k - 1])
+                k++;
+        }
+        else if (arr1[i] < arr2[j])
+            i++;
+        else if (arr2[j] < arr3[k])
+            j++;
+        else
+            k++;
+    }
+
+    return common;
+
+    }
+};
