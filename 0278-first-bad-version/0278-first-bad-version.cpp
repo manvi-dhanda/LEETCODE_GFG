a36da940dec6d316c9936168588a9c761f6b4ddd
Manvi Dhanda <149007210+manvi-dhanda@users.noreply.github.com>
2026-02-26 12:24:56 +0530

    Time: 2 ms (56.2%), Space: 7.9 MB (71.54%) - LeetHub

---
+++ 0278-first-bad-version/0278-first-bad-version.cpp
@@ -0,0 +1,23 @@
+/* The isBadVersion API is defined for you.
+   bool isBadVersion(int version); */
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        
+        long long low = 1;
+        long long high = n;
+
+        while(low < high) {
+
+            long long mid = low + (high - low) / 2;
+
+            if(isBadVersion(mid))
+                high = mid;      
+            else
+                low = mid + 1;   
+        }
+
+        return low;
+    }
+};
